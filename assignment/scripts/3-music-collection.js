68189f95aadaf94019483dccaafad81bee45e769
Jacob Birkeland <birkeland02@gmail.com>
2021-09-16 23:53:49 -0500

    Finally got function addToCollection to work and add new album to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,37 +1,31 @@
 console.log('***** Music Collection *****')
 
-let collection = {};
+let collection = [];
 // Created object literal with empty array
 console.log(collection);
 // Console log newly created object literal
 
-function addToCollection (albumTitle, albumArtist, albumYearPublished){
+function addToCollection (title,artist,yearPublished){
 // Created new function called addToCollection with 3 input parameters called title,artist, and yearPublished
+console.log('in addToCollection', title,artist,yearPublished);
+// Console log to show we are in 'addToCollection'
+    let album = {
+    title: title,
+    // Create new object 'title' and added to collection array
+    artist: artist,
+    // Create new object 'artitst' and added to collection array
+    yearPublished: yearPublished}
+    // Create new object 'yearPublished' and added to collection array
 
-let album = {
-  title:albumTitle,
-  // Create new object 'title' and added to collection array
-  artist:albumArtist,
-  // Create new object 'artitst' and added to collection array
-  yearPublished:albumYearPublished
-  // Create new object 'yearPublished' and added to collection array
-
+    collection.push(album);
+    // pushing added album up to collection
+    return collection;
+    // end addToCollection
 }
-
-  console.log('in addToCollection', albumTitle, albumArtist, albumYearPublished);
-  // Console log to show we are in 'addToCollection'
-
-  collection.push(album);
-
-  return collection;
-
-}
-
-
 
 addToCollection('Let It Be','The Replacements','1985');
-
-
+console.log(collection);
+addToCollection('How to Design a Nervous Breakdown','The Anniversary','2003');
 console.log(collection);
 
-'Let It Be','The Replacements','1985'
+// Super frustrated but happier now - Worked 3 hours on this and it was just a Stupid synax error in my first line - Lesson Learned.
